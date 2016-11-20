600d50c25a844812b821ffc3b7ed6a2dbc35ebb1
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-11-20 16:58:23 +0100

    fixed classes

--- tests/TestCase/Utility/TokensTraitTest.php
+++ tests/TestCase/Utility/TokensTraitTest.php
@@ -20,12 +20,10 @@
  * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link        http://git.novatlantis.it Nova Atlantis Ltd
  */
-namespace Tokens\Controller\Component;
+namespace Tokens\Test\TestCase\Utility;
 
-use Cake\Controller\Component;
-use Tokens\Utility\TokenTrait;
+use Cake\TestSuite\TestCase;
 
-class TokenComponent extends Component
+class TokenTraitTest extends TestCase
 {
-    use TokenTrait;
 }
